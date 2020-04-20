d91d3e35542d1a4a887ba3a9d82a43f055ec0c52
Hyungjun Kim <hjkim@sollae.co.kr>
2020-04-20 13:40:54 +0900

    2020.4.20 - testing auto-update.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("path");
 const { description, version } = require("../package");
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require("electron-squirrel-startup")) {
-  // eslint-disable-line global-require
-  app.quit();
-}
+
+const log = require("electron-log");
+const { autoUpdater } = require("electron-updater");
+
+//-------------------------------------------------------------------
+// Logging
+//
+// THIS SECTION IS NOT REQUIRED
+//
+// This logging setup is not required for auto-updates to work,
+// but it sure makes debugging easier :)
+//-------------------------------------------------------------------
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = "info";
+log.info("App starting...");
+
 let mainWindow;
 const createWindow = () => {
   // Create the browser window.
@@ -23,7 +34,57 @@ const createWindow = () => {
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
+  createDefaultUpdaetWindow();
+  autoUpdater.checkForUpdates();
 };
+
+//------------------------------------------------------------
+// For auto-update
+let updateWin;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  updateWin.webContents.send("message", text);
+}
+
+function createDefaultUpdaetWindow() {
+  updateWin = new BrowserWindow({
+    backgroundColor: "#eeeeee",
+    webPreferences: { nodeIntegration: true },
+  });
+  updateWin.webContents.openDevTools();
+  updateWin.on("closed", () => {
+    updateWin = null;
+  });
+  updateWin.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+  return updateWin;
+}
+
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+autoUpdater.on("update-available", (info) => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+  sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", (err) => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on("update-downloaded", (info) => {
+  sendStatusToWindow("Update downloaded");
+  autoUpdater.quitAndInstall();
+});
+//------------------------------------------------------------
+
+//------------------------------------------------------------
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
